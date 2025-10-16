1d7f8a163e6e3d44e6e6e9ccbf077e3e64888a58
Erick Olachea <erick.olachea@uabc.edu.mx>
2025-10-15 21:58:12 -0700

    update
    todo traducido solo falta agregar educacion

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import { PROJECTS } from "../data/projects"
 import { trackProjectClick } from "../analytics/gaEvents";
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from "react-i18next";
@@ -15,22 +14,16 @@ export default function Projects() {
   
   return (
     <section id="projects" className="mt-12">
-      <h2 className="text-2xl font-bold mb-6">{web.projects}</h2>
+      <h2 className="text-2xl font-bold mb-6">{web.projects.name}</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {PROJECTS.map((p, i) => (
+        {web.projects.proyectname.map((p, i) => (
           <article key={i} className="p-6 rounded-2xl bg-[rgba(255,255,255,0.02)] border border-[rgba(255,255,255,0.03)] backdrop-blur-sm shadow-lg shadow-blue-500/20">
             <div className="flex items-start justify-between">
               <div>
-                <div className="text-lg font-semibold">{p.title}</div>
-                <p className="text-sm text-gray-300 mt-2">{p.desc}</p>
-               
-                <p className="text-sm font-semibold text-white mt-3">Tipos de {p.tipo}
-                  {p.type.map((t) => <p  key={t}  className="text-xs text-gray-300 mt-1"> ▪ {t}</p>)}
-                </p>
-                 <p className="text-sm font-semibold text-white mt-3">Experiencia En {p.tipo}
-                  {p.Exp.map((t) => <p  key={t}  className="text-xs text-gray-300 mt-1"> ▪ {t}</p>)}
-                </p>
-                
+                <div className="text-lg font-semibold mb-2">{p.title}</div>
+                <p className="text-sm font-semibold text-white mt-3">{p.rol}</p>
+                <p className="text-sm text-gray-300 mt-2 mb-3">{p.desc}</p>
+               {p.responsibilities.map((t) => <p  key={t}  className="text-xs text-gray-300 mt-1"> ▪ {t}</p>)}
                 <div className="mt-4 flex flex-wrap gap-2">
                   {p.tech.map((t) => <span key={t} className="px-2 py-1 rounded-md bg-[rgba(255,255,255,0.03)] text-xs">{t}</span>)}
                 </div>
@@ -42,7 +35,7 @@ export default function Projects() {
                   className="text-sm inline-flex items-center gap-2 px-3 py-2 bg-sky-600 rounded-md hover:bg-sky-700"
                   onClick={() => trackProjectClick(p.name)}
                 >
-                  Ver
+                  {p.btn}
                 </Link>
               </div>
             </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-export const PROJECTS = [
+[
   {
     id:2,
     title: 'Softtek - Citigroup',
@@ -7,7 +7,6 @@ export const PROJECTS = [
     tipo:'Pruebas',
     desc: 'Se Trabajo bajo Software Testing Life Cycle (STLC) en pruebas automatizadas en API REST y pruebas manuales para microservicios asincronos, tambien pruebas manuelas en el UI de un ejecutivo de banco',
    "responsibilities": [
-        "Pruebas manuales",
         "Experiencia en el sector bancario",
         "Fundamentos ISTQB",
         "Pruebas funcionales",
@@ -22,8 +21,12 @@ export const PROJECTS = [
     sector:'Logistico y de trasnportacion',
     tipo:'Pruebas',
     desc: 'Se Trabajo bajo Software Testing Life Cycle (STLC) en pruebas manuales de aplicaciones de logistica y transporte',
-    type:['Functional Testing.', 'Regression Testing.', 'Manual Testing.', 'API Testing.', 'Integration Testing.', 'Automation Testing.', 'Smoke Testing.'],
-   Exp:['Analisis y Diseño de Matriz de prueba., Escenarios de prueba y Casos de Prueba.', 'Ejecucion de casos de prueba.', 'Gestion de Defectos.', 'Reuniones una vez por semana con el Cliente.'],
+   "responsibilities": [
+        "Experiencia en el sector logístico y de transportación",
+        "Fundamentos ISTQB",
+        "Pruebas funcionales",
+        "Automatización"
+      ],
     tech: ['Cypress', 'Postman', 'MuleSoft', 'SalesForce', 'QAComplete', 'Excel'],
     btn:"ver"
   },
@@ -34,7 +37,7 @@ export const PROJECTS = [
     sector:'Aplicaciones Web',
     tipo:'Pruebas y Desarrollo',
     desc: 'Desarrolle invitaciones digitales para bodas, con un lado admin para gestionar invitados y tener un mejor control del evento, el otro lado del invitado muestra una invitacion personalizada con fotos e imagenes de los interesados con la informacion correspondiente.',
-    "responsibilities": [
+     "responsibilities": [
         "Pruebas funcionales",
         "Pruebas automatizadas E2E",
         "Desarrollo de aplicaciones web responsivas",
@@ -52,13 +55,12 @@ export const PROJECTS = [
     sector:'Videojuegos 2D',
     tipo:'Desarrollo',
     desc: 'Se desarrollo un video juego en 2D para la Universidad Autonoma De Baja California Campus Ensenada, en dode el video juego es ludico, enseñando a los niños sus derechos y obligaciones ',
-    "responsibilities": [
-        "Pruebas funcionales",
-        "Pruebas automatizadas E2E",
-        "Desarrollo de aplicaciones web responsivas",
-        "Pruebas manuales",
-        "Pruebas de API",
-        "Gestión de bases de datos"
+     "responsibilities": [
+        "Desarrollo de aplicaciones web",
+        "Desarrollo móvil",
+        "Desarrollo de videojuegos",
+        "Bases de datos",
+        "Programación orientada a objetos"
       ],
     tech: ['Unity','C#','Visual Studio'],
     btn:"ver"
@@ -70,8 +72,13 @@ export const PROJECTS = [
     sector:'Ventas y Comida',
     tipo:'Desarrollo',
     desc: 'Se Trabajo en el desarrollo de una aplicacion web y movil para la cafeteria de la Universidad Autonoma De Baja California Campus Ensenada, esto para optimizar la comanda de pedidos y asi los estudiantes puedan ordenar por la app. ',
-    type:['Desarrollo para aplicaciones web.', 'Desarrollo Móvil (Android y IOS).'],
-    Exp:['Análisis y diseño de las historias de usuario.', 'Desarrollo móvil usando Java y Swift.','Desarrollo de Bases de Datos utilizando MySQL.','Desarrollo web utilizando JQuery, PHP, HTML y framework Bootstrap.'],
+   "responsibilities": [
+        "Desarrollo de aplicaciones web",
+        "Desarrollo móvil",
+        "Bases de datos",
+        "Programación orientada a objetos"
+      ],
+    
     tech: ['Trello', 'Postman','JMeter', 'PHP', 'HTML', 'Java', 'AndroidStudio', 'MySQL' , 'JQuery', 'Visual Studio Code'],
     btn:"ver"
   }
